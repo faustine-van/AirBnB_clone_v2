76273b36fe2d69ebcd0cd70d8c14242c04309e2f
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-05 22:35:03 +0000

    updated

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,38 @@
+# sets up your web servers for the deployment of web_static
+
+package { 'nginx':
+  ensure => 'present',
+}
+-> exec { 'create folder for file':
+  command  => 'mkdir -p /data/web_static/releases/test/',
+  provider => 'shell',
+}
+
+-> exec { 'create folder for shared':
+  command  => 'mkdir -p /data/web_static/shared/',
+  provider => 'shell',
+}
+-> file { '/data/web_static/releases/test/index':
+  ensure  => 'file',
+  content => 'Holberton School',
+}
+
+-> exec { 'create symbolic link':
+  command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+  provider => 'shell',
+}
+
+-> exec { 'change owner':
+  command  => 'chown -hR ubuntu:ubuntu /data/',
+  provider => 'shell',
+}
+
+-> exec { 'add to the server to serve content':
+  command  => 'sed -i "51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+-> exec { 'restart server':
+  command  => 'service nginx restart',
+  provider => 'shell',
+}
