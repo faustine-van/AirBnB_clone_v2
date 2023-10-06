989c4bac17ce8186e3ce32a368de3e272c66482c
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-06 12:31:10 +0000

    updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,12 +15,19 @@ def def do_deploy(archive_path):
         return False
     put(archive_path, '/tmp/')
     run('mkdir -p /data/web_static/releases/web_static_20231005220524/')
+
     # extract all files to the folder
+    path = '/data/web_static/releases/web_static_20231005220524/'
+    run(f'tar -xzf /tmp/web_static_20231005220524.tgz -C {path}')
+
+    f_file = '/data/web_static/releases/web_static_20231005220524/web_static/*'
     final_path = '/data/web_static/releases/web_static_20231005220524/'
-    run(f'tar -xzf /tmp/web_static_20231005220524.tgz -C {final_path}')
+    run(f'mv {f_file} {final_path}')
     # delete arhcive
     run('rm -rf /tmp/web_static_20231005220524.tgz')
+    r_file = '/data/web_static/releases/web_static_20170315003959/web_static'
+    run(f'rm -rf {r_file}')
     # delete symbolic link
-    run('rm /data/web_static/current')
+    run('rm -rf /data/web_static/current')
     # create new the symbolic link /data/web_static/current
     run(f'ln -sf {final_path} /data/web_static/current')
