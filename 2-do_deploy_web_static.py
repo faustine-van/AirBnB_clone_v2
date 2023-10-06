eeaba5ab6fea8a5f93ea0d030b260999a2e12296
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-06 13:43:39 +0000

    updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,33 +1,49 @@
-#!/usr/bin/env bash
+#!/usr/bin/python3
 """distributes an archive to your web servers, using the function do_deploy
 """
-from os import os.path
+import os
 from fabric.api import *
 
-env.hosts = ['316762-web-01', '316762-web-02']
+env.hosts = ['18.234.105.144', '54.165.39.106']
 env.user = 'ubuntu'
 
+# define variables
+path = '/data/web_static/releases/web_static_20231005220524/'
+f_file = '/data/web_static/releases/web_static_20231005220524/web_static/*'
+final_path = '/data/web_static/releases/web_static_20231005220524/'
+r_file = '/data/web_static/releases/web_static_20231005220524/web_static'
 
-def def do_deploy(archive_path):
+
+def do_deploy(archive_path):
     """distributes an archive to your web servers"""
 
-    if not exists(archive_path):
+    # check if path exists
+    if not os.path.exists(archive_path):
         return False
+
+    # uploading all file
     put(archive_path, '/tmp/')
-    run('mkdir -p /data/web_static/releases/web_static_20231005220524/')
-
+    new = run('mkdir -p /data/web_static/releases/web_static_20231005220524/')
+    if new.failed:
+        return False
     # extract all files to the folder
-    path = '/data/web_static/releases/web_static_20231005220524/'
-    run(f'tar -xzf /tmp/web_static_20231005220524.tgz -C {path}')
+    extract = run(f'tar -xzf /tmp/web_static_20231005220524.tgz -C {path}')
+    if extract.failed:
+        return False
 
-    f_file = '/data/web_static/releases/web_static_20231005220524/web_static/*'
-    final_path = '/data/web_static/releases/web_static_20231005220524/'
-    run(f'mv {f_file} {final_path}')
+    # mv file
+    mv_file = run(f'mv {f_file} {final_path}')
+    if mv_file.failed:
+        return False
+
     # delete arhcive
     run('rm -rf /tmp/web_static_20231005220524.tgz')
-    r_file = '/data/web_static/releases/web_static_20170315003959/web_static'
     run(f'rm -rf {r_file}')
     # delete symbolic link
+
     run('rm -rf /data/web_static/current')
+
     # create new the symbolic link /data/web_static/current
     run(f'ln -sf {final_path} /data/web_static/current')
+
+    return True
