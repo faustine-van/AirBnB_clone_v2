945c228f02d9626cc84ed07038aa83f6eedb49bb
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-06 08:16:49 +0000

    updated

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -22,10 +22,6 @@ package { 'nginx':
   provider => 'shell',
 }
 
--> exec { 'change owner':
-  command  => 'chown -hR ubuntu:ubuntu /data/',
-  provider => 'shell',
-}
 
 -> exec { 'add to the server to serve content':
   command  => 'sed -i "51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-available/default',
