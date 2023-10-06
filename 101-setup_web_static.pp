fd895081e7f6451529c93154cba2eeed3922dc0b
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-06 07:46:17 +0000

    updated

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,7 +1,7 @@
 # sets up your web servers for the deployment of web_static
 
 package { 'nginx':
-  ensure => 'present'
+  ensure => 'present',
 }
 -> exec { 'create folder for file':
   command  => 'mkdir -p /data/web_static/releases/test/',
@@ -31,7 +31,8 @@ package { 'nginx':
   command  => 'sed -i "51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}" /etc/nginx/sites-available/default',
   provider => 'shell',
 }
--> service { 'nginx':
-  ensure  => running,
-  reqiure => Package['nginx'],
+
+-> exec { 'restart server':
+  command  => 'service nginx restart',
+  provider => 'shell',
 }
