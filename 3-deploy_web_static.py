263462a93977c6e21dcb403888626eccb563d3dd
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-07 18:10:05 +0000

    updated

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,7 +9,8 @@ env.hosts = ['18.234.105.144', '54.165.39.106']
 
 @task
 def do_pack():
-    """archiveall files"""
+    """ archive all files
+    """
     time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     name = "versions/web_static_{}.tgz".format(time)
     version = local("mkdir -p versions")
@@ -21,58 +22,48 @@ def do_pack():
 
 @task
 def do_deploy(archive_path):
-    """distributes an archive to your web servers"""
+    """distributes an archive to your web servers
+    args:
+    archcive_path: file that is archive file
+
+    """
 
     # check if path exists
-    if not os.path.exists(archive_path) is False:
+    if exists(archive_path) is False:
         return False
-    name = archive_path.split('/')
-    f_name = name[-1].split('.')
+    try:
+        name = archive_path.split('/')
+        f_name = name[-1].split('.')
 
-    # define variables
-    path = f'/data/web_static/releases/{f_name[0]}/'
-    f_file = f'/data/web_static/releases/{f_name[0]}/web_static/*'
-    final_path = '/data/web_static/releases/{f_name[0]}/'
-    r_file = '/data/web_static/releases/{f_name[0]}/web_static'
+        # define variables
+        path = f'/data/web_static/releases/{f_name[0]}/'
+        f_file = f'/data/web_static/releases/{f_name[0]}/web_static/*'
+        final_path = '/data/web_static/releases/{f_name[0]}/'
+        r_file = '/data/web_static/releases/{f_name[0]}/web_static'
 
-    # uploading all file
-    upload = put(archive_path, '/tmp/')
-    if upload.failed:
+        # uploading all file
+        put(archive_path, '/tmp/')
+        run(f'mkdir -p /data/web_static/releases/{f_name[0]}/')
+        # extract all files to the folder
+        run(f'tar -xzf /tmp/{f_name[0]}.tgz -C {path}')
+        # delete arhcive
+        run('rm -rf /tmp/{f_name[0]}.tgz')
+        # mv file
+        run(f'mv {f_file} {final_path}')
+        run(f'rm -rf {r_file}')
+        # delete symbolic link
+        run('rm -rf /data/web_static/current')
+        # create new the symbolic link /data/web_static/current
+        run(f'ln -sf {final_path} /data/web_static/current')
+        return True
+    except Exception:
         return False
-
-    if run(f'mkdir -p /data/web_static/releases/{f_name[0]}/').failed is True:
-        return False
-
-    # extract all files to the folder
-    if run(f'tar -xzf /tmp/{f_name[0]}.tgz -C {path}').failed is True:
-        return False
-
-    # delete arhcive
-    if run('rm -rf /tmp/{f_name[0]}.tgz').failed is True:
-        return False
-
-    # mv file
-    if run(f'mv {f_file} {final_path}').failed is True:
-        return False
-
-    if run(f'rm -rf {r_file}').failed is True:
-        return False
-    # delete symbolic link
-
-    if run('rm -rf /data/web_static/current').failed is True:
-        return False
-
-    # create new the symbolic link /data/web_static/current
-    if run(f'ln -sf {final_path} /data/web_static/current').failed is True:
-        return False
-
-    return True
 
 
 def deploy():
-    """archive file and distributes an archive to your web servers"""
+    """archive file and distributes an archive to your web servers
+    """
     archive = do_pack()
     if not archive:
         return False
-    else:
-        return do_deploy(archive)
+    return do_deploy(archive)
