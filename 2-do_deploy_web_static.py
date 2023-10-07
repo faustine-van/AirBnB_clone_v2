7b7cdb344d033d4b27cf584389c3272e02e56963
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-07 11:46:26 +0000

    updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""distributes an archive to your web servers, using the function do_deploy
-"""
+# distributes an archive to your web servers, using the function do_deploy
 import os
 from fabric.api import *
 
