398bd5bdc686494063d205a133b85bf06e1180c9
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-08 20:07:19 +0000

    updated

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """distributes an archive to your web servers, using the function do_deploy"""
 
-import os
+from os.path import exists
 from fabric.api import *
 
 env.hosts = ['18.234.105.144', '54.165.39.106']
@@ -22,11 +22,7 @@ def do_pack():
 
 @task
 def do_deploy(archive_path):
-    """distributes an archive to your web servers
-    args:
-    archcive_path: file that is archive file
-
-    """
+    """distributes an archive to your web servers"""
 
     # check if path exists
     if exists(archive_path) is False:
@@ -38,7 +34,7 @@ def do_deploy(archive_path):
         # define variables
         path = f'/data/web_static/releases/{f_name[0]}/'
         f_file = f'/data/web_static/releases/{f_name[0]}/web_static/*'
-        final_path = '/data/web_static/releases/{f_name[0]}/'
+        # final_path = '/data/web_static/releases/{f_name[0]}/'
         r_file = '/data/web_static/releases/{f_name[0]}/web_static'
 
         # uploading all file
@@ -47,14 +43,15 @@ def do_deploy(archive_path):
         # extract all files to the folder
         run(f'tar -xzf /tmp/{f_name[0]}.tgz -C {path}')
         # delete arhcive
-        run('rm -rf /tmp/{f_name[0]}.tgz')
+        run(f'rm -rf /tmp/{f_name[0]}.tgz')
         # mv file
-        run(f'mv {f_file} {final_path}')
+        run(f'mv {f_file} /data/web_static/releases/{f_name[0]}/')
         run(f'rm -rf {r_file}')
         # delete symbolic link
         run('rm -rf /data/web_static/current')
         # create new the symbolic link /data/web_static/current
-        run(f'ln -sf {final_path} /data/web_static/current')
+        current = '/data/web_static/current'
+        run(f'ln -sf /data/web_static/releases/{f_name[0]}/ {current}')
         return True
     except Exception:
         return False
@@ -64,6 +61,6 @@ def deploy():
     """archive file and distributes an archive to your web servers
     """
     archive = do_pack()
-    if not archive:
+    if archive is None:
         return False
     return do_deploy(archive)
