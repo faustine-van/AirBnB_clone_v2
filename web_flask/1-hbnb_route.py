683666c2d1c0c65be6b54b47a32654a91a5e2581
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-20 11:28:23 +0300

    updated

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """starts a Flask web application"""
 
-from flask import Flask
-
-app = Flask('__name__')
-
+from web_flask import app
 
 @app.route('/', strict_slashes=False)
 def home():
