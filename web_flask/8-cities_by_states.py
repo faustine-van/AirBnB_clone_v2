940a53b60ec176528a7ecab601dd19e05a419111
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-21 23:57:08 +0300

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -20,18 +20,12 @@ def teardown_appcontext(Exception):
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """
-      - H1 tag: “States”
-        - UL tag: with the list of all State objects present
-            in DBStorage sorted by name (A->Z) tip
-         - LI tag: description of one State: <state.id>:
-               <B><state.name></B> + UL tag: with the list
-              of City objects linked to the State sorted by name (A->Z)
-          - LI tag: description of one City: <city.id>: <B><city.name></B>
+         cities_by_states
     """
     states = storage.all('State')
 
     # An empty dict to store all cities
-    return render_template('8-cities_by_states.html', Table="States",  states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == "__main__":
