64b65d57ca4c5a23cdd9b0b3bf687b17c68203d4
faustine-van <faustinemuhayemariya44@gmail.com>
2023-10-21 21:00:30 +0300

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def teardown_appcontext(Exception):
+    """ remove the current SQLAlchemy Session  """
     storage.close()
 
 
@@ -20,9 +21,9 @@ def teardown_appcontext(Exception):
 def cities_by_states():
     """
       - H1 tag: “States”
-        - UL tag: with the list of all State objects present 
+        - UL tag: with the list of all State objects present
             in DBStorage sorted by name (A->Z) tip
-         - LI tag: description of one State: <state.id>: 
+         - LI tag: description of one State: <state.id>:
                <B><state.name></B> + UL tag: with the list
               of City objects linked to the State sorted by name (A->Z)
           - LI tag: description of one City: <city.id>: <B><city.name></B>
@@ -31,7 +32,7 @@ def cities_by_states():
     all_states = sorted(states.values(), key=lambda state: state.name)
 
     # An empty dict to store all cities
-    citiesByState = {}
+    citiesState = {}
     for state in all_states:
         if os.getenv('HBNB_TYPE_STORAGE') == 'db':
             # Use the cities relationship if using DBStorage
@@ -40,9 +41,9 @@ def cities_by_states():
             # Use the public getter method if not using DBStorage
             cities = state.cities()
         cities = sorted(cities, key=lambda city: city.name)
-        citiesByState[state] = cities
-    return render_template('8-cities_by_states.html', citiesByState=citiesByState)
+        citiesState[state] = cities
+    return render_template('8-cities_by_states.html', citiesState=citiesState)
 
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
